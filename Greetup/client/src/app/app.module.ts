49a45d2fadead1bab8c2583ae8b93bee441f4bfc
meltedfork <salerno.suz@gmail.com>
2018-04-24 22:44:35 -0500

    phase 1 of implementing google map

--- Greetup/client/src/app/app.module.ts
+++ Greetup/client/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { HttpModule } from '@angular/http';
-
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { LoginregComponent } from './loginreg/loginreg.component';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +16,7 @@ import { VieweventComponent } from './viewevent/viewevent.component';
 import { MapComponent } from './map/map.component';
 
 // import { PersistenceModule } from 'angular-persistence';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -32,10 +33,16 @@ import { MapComponent } from './map/map.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     HttpModule,
 
     // PersistenceModule,
 
+    AgmCoreModule.forRoot({
+      apiKey:"AIzaSyBQ2xKMzTUY7TDBuiKephoqpxXajWb2_9s",
+      libraries: ["places"]
+    })
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
--- Greetup/client/src/app/map/map.component.ts
+++ Greetup/client/src/app/map/map.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { MapsAPILoader } from '@agm/core';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
 
-  constructor() { }
+export class MapComponent implements OnInit {
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+  public formatted_address: string;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) { }
 
   ngOnInit() {
+    //set google maps defaults
+    this.zoom = 14;
+    this.latitude = 41.8781,
+      this.longitude = -87.6298;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.formatted_address = place.formatted_address;
+          this.zoom = 14;
+          console.log(this.formatted_address);
+          console.log(this.latitude);
+          console.log(this.longitude);
+          console.log(this.zoom);
+
+        });
+      });
+    });
   }
 
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        //this.zoom = 14;
+      });
+    }
+  }
 }
