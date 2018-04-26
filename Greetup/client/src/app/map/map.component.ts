bec06e5abbea5b36dffb7afd57cb0b7a04afbfb2
meltedfork <salerno.suz@gmail.com>
2018-04-26 08:27:19 -0500

    additional map features available

--- Greetup/client/src/app/map/map.component.ts
+++ Greetup/client/src/app/map/map.component.ts
@@ -34,50 +34,15 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     //set google maps defaults
-    this.getEvents();
-    this.zoom = 14;
+    this.zoom = 8;
     this.lat = 41.8781,
     this.lng = -87.6298;
-
-    //create search FormControl
-    this.searchControl = new FormControl();
+    this.getEvents();
 
     //set current position
-    this.setCurrentPosition();
+    //this.setCurrentPosition();
 
-
-    //load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
-
-      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-        types: ["address"]
-      });
-      autocomplete.addListener("place_changed", () => {
-        this.ngZone.run(() => {
-          //get the place result
-          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-
-          //verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          // set latitude, longitude and zoom
-          this.lat = place.geometry.location.lat();
-          this.lng = place.geometry.location.lng();
-          this.formatted_address = place.formatted_address;
-          this.zoom = 14;
-          console.log(this.formatted_address);
-          console.log(this.lat);
-          console.log(this.lng);
-          console.log(this.zoom);
-
-        });
-      });
-    });
   }
-
   
   getEvents(): void {
     console.log("Got to getevents in home.ts");
@@ -87,16 +52,16 @@ export class MapComponent implements OnInit {
       console.log("list of markers:", this.markers)
     })
   }
-
-  private setCurrentPosition() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.lat = position.coords.latitude;
-        this.lng = position.coords.longitude;
-        //this.zoom = 14;
-      });
-    }
-  }
+  // TO DO: try to use this function for additional feature: 'find events near you"
+  // private setCurrentPosition() {
+  //   if ("geolocation" in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.lat = position.coords.latitude;
+  //       this.lng = position.coords.longitude;
+  //       //this.zoom = 14;
+  //     });
+  //   }
+  // }
 }
 interface marker {
   lat: number;
